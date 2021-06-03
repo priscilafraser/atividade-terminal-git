fc7356b4ac5004c9dc336992974d5153f8c5496b
priscila.fraser@gmail.com <priscila.fraser@gmail.com>
2021-06-02 20:41:16 -0400

    Adicionei 2 novos arquivos

---
+++ exercicio1.py
@@ -0,0 +1,14 @@
+Ll = [5, 7, 2, 9, 4, 1, 3]
+
+print(len(Ll))
+print(max(Ll))
+print(min(Ll))
+print(sum(Ll))
+
+Ll.sort()
+print(Ll)
+
+Ll.reverse()
+print(Ll)
+
+#L.sort(reverse = True).print(reverse = True)
---
+++ exercicio2.py
@@ -0,0 +1,16 @@
+frases = []
+
+frases.append("Telefonou para a vítima?")
+frases.append("Esteve no local do crime?")
+frases.append("Mora perto da vítima?")
+frases.append("Devia para a vítima?")
+frases.append("Já trabalhou com a vítima?")
+
+
+
+if 
+
+print(frases)
+
+
+
